9ec4df03cdb71cf7b857a496f9cf18783581ce52
123tabz <armypeshy@gmail.com>
2023-06-29 10:34:24 -0700

    usng printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h> 
 /**
- * main - using the put function
- * Return: 0 (success)
- */
-int main(void):
+ * main - using puts function
+*Returns:0 (success)
+*/
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
